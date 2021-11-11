fa4797ab2c01ba180ea6615226e262c4a4db5ae1
Daniel Olsen <daniel.olsen@iths.se>
2021-11-11 14:09:16 +0100

    b

--- site.js
+++ site.js
@@ -1,1 +1,8 @@
-﻿document.getElementById("answer").innerHTML = "Hello Jasdaript!";
+﻿var out = document.getElementById("answer");
+var btn1 = document.getElementById("btn1");
+var input = document.getElementById("input");
+
+btn1.addEventListener("click", function (e) {
+    out.innerHTML = "Tjena " + input.value + "!";
+
+});
