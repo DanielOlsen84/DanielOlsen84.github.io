29a9442e7aa59fda0b06cf40f1f7b929a2e6cbca
Daniel Olsen <daniel.olsen@iths.se>
2021-11-11 14:24:43 +0100

    c

--- site.js
+++ site.js
@@ -1,8 +1,16 @@
 ﻿var out = document.getElementById("answer");
 var btn1 = document.getElementById("btn1");
 var input = document.getElementById("input");
+var canvas = document.getElementById("canvas");
+var ctx = canvas.getContext("2d");
 
 btn1.addEventListener("click", function (e) {
     out.innerHTML = "Tjena " + input.value + "!";
 
+    ctx.moveTo(0, 0);
+    ctx.lineTo(200, 100);
+    ctx.stroke();
+    ctx.moveTo(100, 0);
+    ctx.lineTo(0, 200);
+    ctx.stroke();
 });
